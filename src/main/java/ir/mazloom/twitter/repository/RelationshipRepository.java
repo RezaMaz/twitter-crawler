a84cd18dbb957188512042b5bd59d48fc55fbb65
Mazloom <rmazloom88@gmail.com>
2020-07-28 23:19:51 +0430

    create repository method for get users with seed and follower and following list of user

--- src/main/java/ir/mazloom/twitter/repository/RelationshipRepository.java
+++ src/main/java/ir/mazloom/twitter/repository/RelationshipRepository.java
@@ -3,5 +3,12 @@ package ir.mazloom.twitter.repository;
 import ir.mazloom.twitter.entity.Relationship;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RelationshipRepository extends JpaRepository<Relationship, Long> {
+
+    List<Relationship> findAllByFollowingId(Long id); //getFollowers
+
+    List<Relationship> findAllByFollowerId(Long id); //getFollowings
+
 }
