4e4c8ab516955bf7f6d5a3148bc240946d338cff
Mazloom <rmazloom88@gmail.com>
2020-07-28 23:20:45 +0430

    get follower and following of seed user first and second get other users

--- src/main/java/ir/mazloom/twitter/Crawler.java
+++ src/main/java/ir/mazloom/twitter/Crawler.java
@@ -11,6 +11,8 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 
 import javax.annotation.PostConstruct;
+import javax.persistence.EntityNotFoundException;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Configuration
@@ -22,19 +24,37 @@ public class Crawler {
 
     @PostConstruct
     void init() {
-        userRepository.findAll().forEach(user -> {
+        userRepository.findAllBySeedTrue().forEach(user -> {
             try {
-                getUserInformation(user);
-                getRelationship(user);
+                persistUser(user, twitter.getUserTimeline(user.getScreenName()).get(0).getUser());
+                persistFollowers(user);
+                persistFollowings(user);
+                setFinishStatus(user);
+            } catch (TwitterException e) {
+                e.printStackTrace();
+            }
+        });
+        userRepository.findAllBySeedFalse().forEach(user -> {
+            try {
+                persistUser(user, twitter.getUserTimeline(user.getScreenName()).get(0).getUser());
+                persistFollowers(user);
+                persistFollowings(user);
+                setFinishStatus(user);
             } catch (TwitterException e) {
                 e.printStackTrace();
             }
         });
     }
 
-    private void getUserInformation(User user) throws TwitterException {
-        twitter4j.User twitterUser = twitter.getUserTimeline(user.getScreenName()).get(0).getUser();
-        persistUser(user, twitterUser);
+    private void setFinishStatus(User user) {
+        Optional<User> byId = userRepository.findById(user.getId());
+        User foundUser = byId.orElseThrow(EntityNotFoundException::new);
+
+        if (relationshipRepository.findAllByFollowingId(user.getId()).size() == foundUser.getFollowersCount() &&
+                relationshipRepository.findAllByFollowerId(user.getId()).size() == foundUser.getFriendsCount())
+            user.setFinish(true);
+        else
+            user.setFinish(false);
     }
 
     private void persistUser(User user, twitter4j.User twitterUser) {
@@ -47,22 +67,41 @@ public class Crawler {
         userRepository.saveAndFlush(user);
     }
 
-    private void getRelationship(User user) throws TwitterException {
+    private void persistFollowers(User user) throws TwitterException {
         long cursor = -1L;
         while (true) {
-            PagableResponseList<twitter4j.User> followersList = twitter.getFollowersList(user.getScreenName(), cursor);
-            followersList.forEach(x -> {
-                User follower = new User();
-                persistUser(follower, x);
+            PagableResponseList<twitter4j.User> followerList = twitter.getFollowersList(user.getScreenName(), cursor);
+            followerList.forEach(follower -> {
+                persistUser(new User(), follower);
 
                 Relationship relationship = new Relationship();
                 relationship.setFollowerId(follower.getId());
                 relationship.setFollowingId(user.getId());
                 relationshipRepository.saveAndFlush(relationship);
             });
-            cursor = followersList.getNextCursor();
-            if (!followersList.hasNext())
+            cursor = followerList.getNextCursor();
+            if (!followerList.hasNext()) {
                 break;
+            }
+        }
+    }
+
+    private void persistFollowings(User user) throws TwitterException {
+        long cursor = -1L;
+        while (true) {
+            PagableResponseList<twitter4j.User> followingList = twitter.getFriendsList(user.getScreenName(), cursor);
+            followingList.forEach(following -> {
+                persistUser(new User(), following);
+
+                Relationship relationship = new Relationship();
+                relationship.setFollowerId(user.getId());
+                relationship.setFollowingId(following.getId());
+                relationshipRepository.saveAndFlush(relationship);
+            });
+            cursor = followingList.getNextCursor();
+            if (!followingList.hasNext()) {
+                break;
+            }
         }
     }
 
