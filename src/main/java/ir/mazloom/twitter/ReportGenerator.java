9430e8dcb9bb880f4d9e6c2802144406b6b44375
Mazloom <rmazloom88@gmail.com>
2021-03-23 13:06:45 +0430

    add other account getting to twitter api

--- src/main/java/ir/mazloom/twitter/ReportGenerator.java
+++ src/main/java/ir/mazloom/twitter/ReportGenerator.java
@@ -1,6 +1,5 @@
 package ir.mazloom.twitter;
 
-import ir.mazloom.twitter.entity.Tweet;
 import ir.mazloom.twitter.entity.User;
 import ir.mazloom.twitter.repository.TweetRepository;
 import ir.mazloom.twitter.repository.UserRepository;
@@ -11,10 +10,12 @@ import org.springframework.stereotype.Component;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 
+import javax.annotation.PostConstruct;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -41,30 +42,61 @@ public class ReportGenerator {
         System.out.println("twitter user report finished.");
     }
 
-    void twitterTweetReport() throws IOException {
-        File file = new File("twitter_tweet.txt");
-        List<Tweet> all = tweetRepository.findAll();
-        for (Tweet tweet : all) {
-            FileUtils.writeStringToFile(file,
-                    tweet.getId() + "\n" +
-                            tweet.getUserId() + "\n" +
-                            tweet.getFavoriteCount() + "\n" +
-                            tweet.getRetweetCount() + "\n" +
-                            tweet.getCreatedAt() + "\n" +
-                            tweet.getInReplyToStatusId() + "\n" +
-                            tweet.getInReplyToUserId() + "\n" +
-                            tweet.getText().replaceAll("\n", " ").replaceAll("\r", " ") + "\n"
-                    , Charsets.UTF_8, true);
+    @PostConstruct
+    void twitterTweetReport() {
+        File file = new File("sutfinal.csv");
+        List<String> strings = null;
+        try {
+            strings = FileUtils.readLines(file, Charset.defaultCharset());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        System.out.println("twitter tweet report finished.");
+        strings.forEach(x -> {
+            try (InputStream in = new URL(getFreeTwitterApi().showUser(x.split(",")[2].replaceAll("\"","")).getProfileImageURL().replace("_normal", "")).openStream()) {
+                File file1 = new File("C:\\download\\" + x.split(",")[2].replaceAll("\"","") + ".png");
+                FileUtils.copyInputStreamToFile(in, file1);
+            } catch (TwitterException e) {
+                if (((TwitterException) e).getErrorMessage().equals("User not found."))
+                    System.out.println(x.split(",")[2].replaceAll("\"",""));
+            } catch (IOException e){}
+        });
+
     }
 
-    void downloadImage(Long userId) throws TwitterException, IOException, InterruptedException {
-        try (InputStream in = new URL(twitterAPIs.get(0).showUser(userId).getProfileImageURL().replace("_normal", "")).openStream()) {
-            File file = new File("C:\\download\\" + userId + ".png");
-            FileUtils.copyInputStreamToFile(in, file);
-            Thread.sleep(1000);
+    //        @PostConstruct
+    void downloadImage() {
+        Twitter freeTwitterApi = getFreeTwitterApi();
+        List<User> allBySeedTrue = userRepository.findAllBySeedTrue();
+        for (User user : allBySeedTrue) {
+            try {
+                try (InputStream in = new URL(freeTwitterApi.showUser(user.getId()).getProfileImageURL().replace("_normal", "")).openStream()) {
+                    File file = new File("C:\\download\\" + user.getId() + ".png");
+                    FileUtils.copyInputStreamToFile(in, file);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (TwitterException e) {
+                if (e.getErrorMessage() != null && e.getErrorMessage().equals("Rate limit exceeded")) {
+                    freeTwitterApi = getFreeTwitterApi();
+                }
+            }
         }
+    }
+
+    private Twitter getFreeTwitterApi() {
+        for (int i = 0; i < twitterAPIs.size(); i++) {
+            try {
+                twitterAPIs.get(i).showUser("jzarif");
+                return twitterAPIs.get(i);
+            } catch (TwitterException ignored) {
+            }
+        }
+        try {
+            Thread.sleep(1000 * 1000);
+        } catch (InterruptedException interruptedException) {
+            interruptedException.printStackTrace();
+        }
+        return twitterAPIs.get(0);
     }
 
 }
